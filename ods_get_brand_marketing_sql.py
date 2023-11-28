2adfae05d2295485c67e33fa9a38c1ab80eff793
Trent <dfscashline21@gmail.com>
2023-11-27 17:17:31 -0800

    added emails to brand marketing

--- ods_get_brand_marketing_sql.py
+++ ods_get_brand_marketing_sql.py
@@ -2,7 +2,7 @@
 
 getBrandMarketing =   """
                 select bm.*, ty.LIST_ITEM_NAME , em.EMAIL ,em.FULL_NAME ,em.JOBTITLE , br.BRAND_RECORDS_NAME , br.BILLING_CUSTOMER_ID,vm.VENDOR_ID  , ct.COMPANYNAME , ct.EMAIL
-                , ct.FULL_NAME , bil.LIST_ITEM_NAME as billing_method,sysdate().cnt.EMAIL from Administrator.VENDOR_FUNDING__BRAND_MARKETI bm
+                , ct.FULL_NAME , bil.LIST_ITEM_NAME as billing_method,sysdate(),COALESCE(cnt.EMAIL,cst.EMAIL,vnd.email) from Administrator.VENDOR_FUNDING__BRAND_MARKETI bm
                 left join Administrator.EMPLOYEES em on em.EMPLOYEE_ID = bm.MERCH_MARKETING_CONTACT_ID 
                 left join Administrator.BRAND_RECORDS br on br.BRAND_RECORDS_ID = bm.BRAND_ID 
                 left join Administrator.CUSTOMERS ct on ct.CUSTOMER_ID = br.BILLING_CUSTOMER_ID 
@@ -11,6 +11,9 @@ getBrandMarketing =   """
                 left join Administrator.BRAND_RECORDS_BILLING_VENDOR_MAP vm on vm.BRAND_RECORDS_ID =br.BRAND_RECORDS_ID 
                 left join (select c.brand_records_id as BRAND_RECORDS_ID , Max(c.Contact_id) as CONTACT_ID from Administrator.BRAND_RECORDS_VENDOR_CONTACT_MAP c group by c.BRAND_RECORDS_ID )cm on cm.BRAND_RECORDS_ID  = bm.BRAND_ID 
                 left join (select CONTACT_ID as CONTACT_ID, EMAIL as EMAIL from Administrator.CONTACTS )cnt on cnt.CONTACT_ID = cm.CONTACT_ID
+                left join (select cust.CUSTOMER_ID ,cust.EMAIL from Administrator.CUSTOMERS cust)cst on cst.customer_id = br.billing_customer_id
+                left join (select  vendor_id,email from Administrator.VENDORS ) vnd on vnd.VENDOR_ID = vm.VENDOR_ID 
+
                 """
 
 spPostBrandMarketing = {
@@ -27,8 +30,8 @@ spPostBrandMarketing = {
                       SET 
                       BRAND_MARKETING.BILLING_CUSTOMER_ID = upd.BILLING_CUSTOMER_ID, 
                       BRAND_MARKETING.BILLING_VENDOR_ID  = upd.BILLING_VENDOR_ID, 
-                      BRAND_MARKETING.BILLING_METHOD  = upd.BILLING_METHOD 
-                      BRAND_MARKETING.EMAIL = upd.EMAIL
+                      BRAND_MARKETING.BILLING_METHOD  = upd.BILLING_METHOD ,
+                      BRAND_MARKETING.BRANDEMAIL = upd.BRANDEMAIL
                       FROM (SELECT * FROM staging.STG_BRAND_MARKETING) upd
                       WHERE BRAND_MARKETING.VENDOR_FUNDING__BRAND_MARKE_ID  = upd.VENDOR_FUNDING__BRAND_MARKE_ID"""
                     ,
